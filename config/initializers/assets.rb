655f89199b2bb0e0112a1677f2c240b3586c5aa6
egoruska85 <egorarbuzov@gmail.com>
2022-07-04 15:34:15 +0500

    add font

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += %w( style.css )
 
 
 
+
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
